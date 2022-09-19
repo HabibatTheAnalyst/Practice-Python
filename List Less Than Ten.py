f28db09f70342b7e796505ce3347347e4f9fccf0
Habibat <habibatjimoh9@gmail.com>
2022-09-19 17:06:08 +0100

    code

--- List Less Than Ten.py
+++ List Less Than Ten.py
@@ -1,1 +1,18 @@
+class MyNumbers:
+  def __int__(self):
+    self.a = 1
+    return self
 
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
